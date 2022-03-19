a9ce43fd9a524db40af9aa33ad4a774fbde22572
Ayush Tripathi <yush.pbh@gmail.com>
2022-03-19 20:14:08 +0530

    Fixed Port Issues

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const router = express.Router()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.static('public'))       //for static files like css js img
